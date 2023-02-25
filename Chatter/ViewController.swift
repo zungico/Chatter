9dc8cf8f88f615076d62391521be30c07446369e
Vladimir Alekseev <vovauu@icloud.com>
2023-02-25 08:56:54 +0300

    Initial Commit

---
+++ Chatter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chatter
+//
+//  Created by Вова on 25.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
