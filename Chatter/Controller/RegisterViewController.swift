49bc13fb117815546d4879fe6ca77c788e2eec9f
Vladimir Alekseev <vovauu@icloud.com>
2023-02-25 17:00:56 +0300

    add basic design

---
+++ Chatter/Controller/RegisterViewController.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterViewController.swift
+//  Chatter
+//
+//  Created by Вова on 25.02.2023.
+//
+
+import Foundation
--- Chatter/Controller/WelcomeViewController.swift
+++ Chatter/Controller/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  Chatter
 //
 //  Created by Вова on 25.02.2023.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
